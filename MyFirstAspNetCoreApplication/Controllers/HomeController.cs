d270693d6193e62ffcfd717e632fa26d24eb0a12
Jerome Caling <jerome.caling@dss.asia>
2019-06-20 14:13:36 +0800

    Sort, filter, page, and group

--- MyFirstAspNetCoreApplication/Controllers/HomeController.cs
+++ MyFirstAspNetCoreApplication/Controllers/HomeController.cs
@@ -4,23 +4,47 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MyFirstAspNetCoreApplication.Models;
+using MyFirstAspNetCoreApplication.Data;
+using MyFirstAspNetCoreApplication.Models.SchoolViewModels;
 
 namespace MyFirstAspNetCoreApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<ActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
+            IQueryable<EnrollmentDataGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDataGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
 
-            return View();
+            return View(await data.AsNoTracking().ToListAsync());
         }
+
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
+
+        //    return View();
+        //}
 
         public IActionResult Contact()
         {
