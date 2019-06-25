323eb56e095a83c3027bfdf27e98f5234e0182d7
Jerome Caling <jerome.caling@dss.asia>
2019-06-25 10:16:03 +0800

    Inheritance

--- MyFirstAspNetCoreApplication/Migrations/20190625020225_Inheritance.Designer.cs
+++ MyFirstAspNetCoreApplication/Migrations/20190625020225_Inheritance.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyFirstAspNetCoreApplication.Data;
 
 namespace MyFirstAspNetCoreApplication.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20190625020225_Inheritance")]
+    partial class Inheritance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
